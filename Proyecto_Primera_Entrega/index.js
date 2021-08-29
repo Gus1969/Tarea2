18c989d38188cdec81e281dc0408f1e1ffc7a83d
Gus1969 <80179740+Gus1969@users.noreply.github.com>
2021-08-29 13:27:34 -0300

    java

---
+++ Proyecto_Primera_Entrega/index.js
@@ -0,0 +1,45 @@
+$(window).on('load',function(){
+	setTimeout(function(){ // allowing 3 secs to fade out loader
+	$('.page-loader').fadeOut('slow');
+	},3000);
+});
+
+
+/////////////////////VALIDAR FORMULARIO DE REGISTRO ////////////////////
+function enviarDatos(registro) {
+
+	var re = /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/; 
+		if(!re.test(registro.email.value)){
+	  alert("el email es obligatorio");
+	  return false;
+	  } else
+	
+	 if(registro.password.value.trim().length == 0) {
+		 alert("Ingrese su palabra clave");
+		 return false;
+	 } else
+	 if(registro.password.value != registro.confirmacion.value) {
+		 alert("la confirmacion no coincide");
+		 return false;
+	 }  else
+	 if(registro.género.value == "") {
+		alert("Seleccione una de las opciones");
+		 return false;
+	 } else
+	   if(registro.edades.value  == "") {
+		   document.getElementById('errormsg'). innerHTML = "Ingrese su edad";
+		  return false;
+	  }  else
+	   if (registro.edades.value < 18 &&  registro.edades.value  > 60)  {
+		document.getElementById('errormsg'). innerHTML = " Permiso denegado";
+		return false;
+	}
+	  
+	  if(!registro.condiciones.checked) {
+		  alert('Debe aceptar los terminos y condiciones');
+		  return false;
+	  } else
+	  alert("Se ha registrado correctamente");
+	 return true;
+	}
+	
