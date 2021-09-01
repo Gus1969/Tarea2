7f580f0e5ba76ae7a1e80b52678fc6e0fe4cd022
Gus1969 <80179740+Gus1969@users.noreply.github.com>
2021-09-01 10:29:34 -0300

    funciones2

---
+++ Javascript/index.js
@@ -0,0 +1,13 @@
+// let objeto = {
+//     nombre: 'Gustavo',
+//     edad: 27,
+
+// }
+// console.log(objeto.nombre);
+
+const  numeroAleatorio =  Math.round(Math.random * 10);
+prompt('escriba un numero del 1 al 10');
+
+if(numeroAleatorio == 10) {
+
+}
