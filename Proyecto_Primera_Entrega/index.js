e4bf1675d69bb80caea4e7b6baabf1a3190ab39a
Gus1969 <80179740+Gus1969@users.noreply.github.com>
2021-09-02 18:20:16 -0300

    subida-dinamica-canciones

--- Proyecto_Primera_Entrega/index.js
+++ Proyecto_Primera_Entrega/index.js
@@ -1,8 +1,76 @@
-$(window).on('load',function(){
-	setTimeout(function(){ // allowing 3 secs to fade out loader
-	$('.page-loader').fadeOut('slow');
-	},3000);
+
+var top3 = [];
+var tabla;
+var sonido; 
+$(document).ready(function() { 
+	var suboCanciones = function(){
+	 
+	 var temas = null;
+	
+	//   var reproducciones
+	//   var nombre = "";
+	 $.ajax({
+		 async: false,
+		  url: "datos.json",
+		 success: function(resultado) {
+			temas = resultado.songs; 
+			
+		
+			$("#buscador").on("keyup", function() {
+				var valor = $(this).val().toLowerCase();
+				$("#hits div .col-12").filter(function() {
+				 $(this).toggle($(this).text().toLowerCase().indexOf(valor) > -1)
+				});
+			  });
+				  //songs.sort();
+					temas.sort(function(a, b){
+						return a.reproducciones - b.reproducciones;
+					});
+					temas.reverse();
+					console.log(temas);
+					for(var i = 0; i <= 2; i++){
+						sonido = `<audio controls src="./canciones/${songs[i].ruta}"><p>Su navegador no soporta el elemento de audio.</p></audio>`
+						 tabla += `<tr><td class=".table-body">${songs[i].nombre}</td><td>${sonido}</td></tr>`
+		 }	
+		}
+		
+	 });
+		return temas
+	}();
+	
+
+		var html = ""
+		
+			  for(var i = 0; i < suboCanciones.length; i++)  {                     
+			
+				
+
+			html += `
+					<div class="card text-center">
+					<div class="col-12">
+					<img src='./IMG/imagenes/icono_${suboCanciones[i].icono}.png 'width=55%' alt='cancion'>
+					<br>
+					<h3>${suboCanciones[i].nombre}</h3>
+					<p>Reproducciones: ${suboCanciones[i].reproducciones}</p>
+					<br>
+					<audio controls src="./songs/${suboCanciones[i].ruta}" type="audio/mp3" width='100%'>
+					</audio>
+					</div>
+					</div>
+					`
+				   
+		}
+		document.getElementById('hits').innerHTML = html;
+		
 });
+
+
+
+$(window).on('load',function(){
+	setTimeout(function(){ 
+$('.page-loader').fadeOut('slow');
+	},3000);
+ });
 
 
 /////////////////////VALIDAR FORMULARIO DE REGISTRO ////////////////////
@@ -30,7 +98,7 @@ function enviarDatos(registro) {
 		   document.getElementById('errormsg'). innerHTML = "Ingrese su edad";
 		  return false;
 	  }  else
-	   if (registro.edades.value < 18 &&  registro.edades.value  > 60)  {
+	   if (registro.edades.value  === "joven")  {
 		document.getElementById('errormsg'). innerHTML = " Permiso denegado";
 		return false;
 	}
