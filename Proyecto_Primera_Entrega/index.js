8abd944ea3b59338fcb4975ccac0eed2a90dafbf
Gus1969 <80179740+Gus1969@users.noreply.github.com>
2021-09-09 18:36:13 -0300

    preloader JS

--- Proyecto_Primera_Entrega/index.js
+++ Proyecto_Primera_Entrega/index.js
@@ -1,9 +1,9 @@
 			
-$(window).on('load',function(){
-	setTimeout(function(){ 
-$('.page-loader').fadeOut('slow');
-	},3000);
- });
+// $(window).on('load',function(){
+// 	setTimeout(function(){ 
+// $('.page-loader').fadeOut('slow');
+// 	},3000);
+//  });
 
 
 
