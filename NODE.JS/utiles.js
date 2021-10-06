117ec294217a295e0b249c6d81e491b92a49408f
Gus1969 <gustavo.ubalperez@gmail.com>
2021-10-06 18:14:44 -0300

    NODE..JS  folder

--- NODE.JS/utiles.js
+++ NODE.JS/utiles.js
@@ -1,10 +1,44 @@
-const alumnos = require("./app")
+//const alumnos = require("./app")
 
 // diasHabiles.forEach((e) =>{
 //     console.log(e)
     
 //});
 
-  alumnos.forEach((item) => {
-    console.log(item)
-  });
+  // alumnos.forEach((item) => {
+  //   console.log(item)
+  // });
+
+
+  /////////////////////mas ejemplos, se muestran en app,js/////////////////
+  // Sort
+// Filter
+// Find
+// Map
+// Reverse
+
+
+
+const edades = [10, 20, 18, 40];
+
+const mayoresEdad = edades.filter((item) => item >= 18);
+
+// // 40 18 20
+ console.log(mayoresEdad.reverse());
+
+// //20 18 40
+// console.log(mayoresEdad);
+
+// const persons = [
+//   { firstname: "Malcom", lastname: "Reynolds" },
+//   { firstname: "Kaylee", lastname: "Frye" },
+//   { firstname: "Jayne", lastname: "Cobb" },
+// ];
+
+// function getFullName(item) {
+//   return [item.firstname, item.lastname].join(" ");
+// }
+
+// console.log(persons.map(getFullName));
+
+module.exports = edades
