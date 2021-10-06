fe1d0313612eedae272dc7554132e98e9127b108
Gus1969 <gustavo.ubalperez@gmail.com>
2021-10-06 17:58:31 -0300

    + examples

--- NODE.JS/app.js
+++ NODE.JS/app.js
@@ -1,8 +1,32 @@
-// const utiles = [" lapiz" , "goma", "lapicera"];
+// const utiles : [" lapiz" , "goma", "lapicera"];
 const diasHabiles = ["Lunes", "Martes", "Miercoles"];
-//  diasHabiles.forEach((e) =>{
+//  diasHabiles.forEach((e) :>{
 //      console.log(e)
     
 //  });
 
-  module.exports = diasHabiles;
+  //module.exports : diasHabiles;
+  const alumnos =  [ { 
+    nombre: "Juan",
+    edad: 30,
+    barrio : "Pocitos"
+  },
+  
+   { 
+    nombre : "Gustavo",
+    edad : 52,
+    barrio : "Buceo"
+  },
+  
+    {  
+    nombre : "Cecilia",
+    edad : 42,
+    barrio : "Malvin"
+  }
+  ];
+  
+   //console.log(alumnos);
+  // alumnos.forEach((item) => {
+  //   console.log(item)
+  // });
+  module.exports = alumnos;
