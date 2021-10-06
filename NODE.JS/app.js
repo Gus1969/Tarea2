0368ccd28c60ce2d1bed1430cbd9439a9380c76e
Gus1969 <gustavo.ubalperez@gmail.com>
2021-10-06 18:14:30 -0300

    NODE.JS folder examples

--- NODE.JS/app.js
+++ NODE.JS/app.js
@@ -1,32 +1,35 @@
+const edades = require("./utiles")
 // const utiles : [" lapiz" , "goma", "lapicera"];
-const diasHabiles = ["Lunes", "Martes", "Miercoles"];
+//const diasHabiles = ["Lunes", "Martes", "Miercoles"];
 //  diasHabiles.forEach((e) :>{
 //      console.log(e)
     
 //  });
 
   //module.exports : diasHabiles;
-  const alumnos =  [ { 
-    nombre: "Juan",
-    edad: 30,
-    barrio : "Pocitos"
-  },
+  // const alumnos =  [ { 
+  //   nombre: "Juan",
+  //   edad: 30,
+  //   barrio : "Pocitos"
+  // },
   
-   { 
-    nombre : "Gustavo",
-    edad : 52,
-    barrio : "Buceo"
-  },
+  //  { 
+  //   nombre : "Gustavo",
+  //   edad : 52,
+  //   barrio : "Buceo"
+  // },
   
-    {  
-    nombre : "Cecilia",
-    edad : 42,
-    barrio : "Malvin"
-  }
-  ];
+  //   {  
+  //   nombre : "Cecilia",
+  //   edad : 42,
+  //   barrio : "Malvin"
+  // }
+  // ];
   
    //console.log(alumnos);
   // alumnos.forEach((item) => {
   //   console.log(item)
   // });
-  module.exports = alumnos;
+  //module.exports = alumnos;
+
+  
