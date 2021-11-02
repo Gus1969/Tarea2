869b8e03156642642cab90c3367ffbe506d25489
Gus1969 <gustavo.ubalperez@gmail.com>
2021-11-02 11:43:26 -0300

    app.js accesingGithub

---
+++ Accessing_Github/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const axios = require('axios');
+const PORT = 35000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send ("Servidor running");
+});
+app.get('/repositorios/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const axiosResponse = await axios(`https://api.github.com/users/${userId}`);
+        console.log(axiosResponse);
+        const repositoriosUrl = user.repos_url;
+        const axiosRepos = await axios(repositoriosUrl);  
+    // const repos = await axiosRepos.json();
+
+    const repoNames = [];
+
+    axiosRepos.forEach(repo => {
+      console.log(repo.name);
+      repoNames.push(repo.name);
+    })
+
+    res.send({ mensaje: 'Repositorios obtenidos con exito', repositorios: repoNames });
+        
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+});
+
+
+
+app.listen(35000, (req, res) => {
+    console.log( `app listening on port ${PORT}`);
+});n
+
